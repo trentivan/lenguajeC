e842f8289f4e40472a28faca6efccf14903d22cc
trentivan <141792967+trentivan@users.noreply.github.com>
2023-09-30 22:49:01 -0700

    modified:   taller5/externa.c

--- taller5/externa.c
+++ taller5/externa.c
@@ -1,4 +1,4 @@
-//Nombre del archivo:   taller5.c
+//Nombre del archivo:   externa.c
 //Autor: Hernandez Cesena Ivan Fernando
 //Fecha de creación: 30 de septiembre de 2023
 //Descripción: Este archivo contiene el código principal de mi programa.
