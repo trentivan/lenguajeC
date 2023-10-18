5f1142a3f22ae035791abe9ffa0f544866251994
trentivan <141792967+trentivan@users.noreply.github.com>
2023-10-18 15:44:07 -0700

    modified:   taller6/output/taller6.exe 	modified:   taller6/taller6.c

--- taller6/taller6.c
+++ taller6/taller6.c
@@ -1,14 +1,46 @@
 #include <stdio.h>
 #include "ivan.h"
 
- void apuntador(int *ptr)
- {
-    // int x=2;
-    // int *ptr;
-    // ptr= &x;
-    // printf("x = %d\n", x);
-    printf("ptr = %p\n", ptr);
- }
+void apuntador(int *ptr);
+void multiplicarPorDos(int *num);
+void modificarVector(int vector[], int n, int *apuntarVector);
+
+ int main()
+{
+    int x=4;
+
+    int n=7, i;
+    int vector[n];
+    int *apuntarVector;
+    apuntarVector = &vector;
+    
+    apuntador(&x);
+
+    multiplicarPorDos(&x);
+
+    printf("el = %d\n\n", x);
+    vectorAleatorioSinRepetir(vector, n, 1, 50);
+    for(i=0; i<n; i++)
+    {
+        printf("%d\n", vector[i]);
+        apuntarVector++;
+    }
+    printf("\n");
+
+    modificarVector(vector, n, *apuntarVector);
+
+    for( i = 0; i<n; i++)
+    {
+        printf("%d\n", vector[i]);
+        
+    }
+    return 0;
+}
+
+void apuntador(int *ptr)
+{
+    printf("ptr = %p\n\n", ptr);
+}
 
 void multiplicarPorDos(int *num)
 {
@@ -19,46 +51,10 @@ void multiplicarPorDos(int *num)
 void modificarVector(int vector[], int n, int *apuntarVector)
 {
     int sumar=7, i;
-    apuntarVector= &vector;
 
     for(i=0; i<n; i++)
     {
-        printf("%d", i);
-        vector[*apuntarVector] = vector[*apuntarVector] + sumar;
-        apuntarVector++;
+        apuntarVector=&vector[i];
+        *apuntarVector= *apuntarVector + sumar;
     }
 }
-
- int main()
-{
-    // int *numero;
-    int x=4;
-    // numero = &x;
-
-    int n=7, i;
-    int vector[n];
-    int *apuntarVector;
-    apuntarVector = &vector;
-    
-    apuntador(&x);
-    multiplicarPorDos(&x);
-    printf("el = %d\n", x);
-    vectorAleatorioSinRepetir(vector, n, 1, 50);
-    for(i=0; i<n; i++)
-    {
-        printf("%d\n", *apuntarVector);
-        apuntarVector++;
-    }
-    printf("\n");
-
-    
-    for(i=0; i<n; i++)
-    {
-        modificarVector(vector, n, &apuntarVector);
-        printf("%d\n", *apuntarVector);
-        apuntarVector++;
-    }
-
-    return 0;
-}
-
