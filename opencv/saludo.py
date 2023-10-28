b2026f045f17758f684d491632a99e322a30393e
trentivan <141792967+trentivan@users.noreply.github.com>
2023-10-27 22:38:56 -0700

    new file:   opencv/flor.jpg 	new file:   opencv/saludo.py 	new file:   python/flor.jpg 	new file:   python/saludo.py

---
+++ opencv/saludo.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+import imutils
+
+rojoBajo1 = np.array([0, 140, 90], np.uint8)
+rojoAlto1 = np.array([8, 255, 255], np.uint8)
+rojoBajo2 = np.array([160, 140, 90], np.uint8)
+rojoAlto2 = np.array([180, 255, 255], np.uint8)
+
+
+
+# leer imagen
+
+image = cv2.imread('flor.jpg')
+image = imutils.resize(image, width=640)
+
+# transformando a otros espacios de color
+imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+imageHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+# detectando el color rojo
+maskRojo1 = cv2.inRange(imageHSV, rojoBajo1, rojoAlto1)
+maskRojo2 = cv2.inRange(imageHSV, rojoBajo2, rojoAlto2)
+mask = cv2.add(maskRojo1, maskRojo2)
+
+
+cv2.imshow('image', image)
+cv2.imshow('mask', mask)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
